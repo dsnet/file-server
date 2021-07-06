9af448a4417aa1e1b03679ba0883fd921f01683f
Joe Tsai <joetsai@digital-static.net>
2021-07-05 18:04:35 -0700

    Cleanup flag help
    Insert newlines for help output to print nicer.

--- main.go
+++ main.go
@@ -22,9 +22,9 @@ import (
 
 var (
 	addr    = flag.String("addr", ":8080", "The network address to listen on.")
-	hide    = flag.String("hide", "/[.][^/]+$", "Regular expression of file paths to hide. Paths matching this pattern are excluded from directory listings, but direct fetches for this path are still resolved.")
-	exclude = flag.String("exclude", "", "Regular expression of file paths to exclude. Paths matching this pattern are excluded from directory listings and direct fetches for this path report NotFound.")
-	index   = flag.String("index", "", "Name of the index page to directly render for a directory. (e.g., 'index.html'; default none)")
+	hide    = flag.String("hide", "/[.][^/]+$", "Regular expression of file paths to hide.\nPaths matching this pattern are excluded from directory listings,\nbut direct fetches for this path are still resolved.")
+	exclude = flag.String("exclude", "", "Regular expression of file paths to exclude.\nPaths matching this pattern are excluded from directory listings\nand direct fetches for this path report NotFound.")
+	index   = flag.String("index", "", "Name of the index page to directly render for a directory.\n(e.g., 'index.html'; default none)")
 	root    = flag.String("root", ".", "Directory to serve files from.")
 	verbose = flag.Bool("verbose", false, "Log every HTTP request.")
 
