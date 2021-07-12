d94d466ee8efc17804735b0ebb11e3710500c0a8
Joe Tsai <joetsai@digital-static.net>
2021-07-12 16:43:55 -0700

    Format HTTP errors as HTML

--- main.go
+++ main.go
@@ -25,9 +25,9 @@ import (
 
 var (
 	addr     = flag.String("addr", ":8080", "The network address to listen on.")
-	hide     = flag.String("hide", "/[.][^/]+/?$", "Regular expression of file paths to hide.\nPaths matching this pattern are excluded from directory listings,\nbut direct fetches for this path are still resolved.")
-	deny     = flag.String("deny", "", "Regular expression of file paths to deny.\nPaths matching this pattern are excluded from directory listings\nand direct fetches for this path report StatusForbidden.")
-	index    = flag.String("index", "", "Name of the index page to directly render for a directory.\n(e.g., 'index.html'; default none)")
+	hide     = flag.String("hide", "/[.][^/]+/?$", "Regular expression of file paths to hide.\nPaths matching this pattern are excluded from directory listings,\nbut direct requests for this path are still resolved.")
+	deny     = flag.String("deny", "", "Regular expression of file paths to deny.\nPaths matching this pattern are excluded from directory listings\nand direct requests for this path report StatusForbidden.")
+	index    = flag.String("index", "", "Regular expression of file paths to treat as index.html pages.\n(e.g., '/index[.]html$'; default none)")
 	root     = flag.String("root", ".", "Directory to serve files from.")
 	sendfile = flag.Bool("sendfile", true, "Allow the use of the sendfile syscall.")
 	verbose  = flag.Bool("verbose", false, "Log every HTTP request.")
@@ -103,13 +103,13 @@ func main() {
 		// Verify that the file exists.
 		f, err := dir.Open(filepath.Join(".", filepath.FromSlash(r.URL.Path)))
 		if err != nil {
-			httpError(w, err)
+			httpError(w, r, err)
 			return
 		}
 		defer f.Close()
 		fi, err := f.Stat()
 		if err != nil {
-			httpError(w, err)
+			httpError(w, r, err)
 			return
 		}
 
@@ -126,7 +126,7 @@ func main() {
 
 		// Reject paths that match the deny pattern.
 		if regexpMatch(denyRx, r.URL.Path) {
-			httpError(w, os.ErrPermission)
+			httpError(w, r, os.ErrPermission)
 			return
 		}
 
@@ -134,7 +134,7 @@ func main() {
 		if fi.IsDir() {
 			serveDirectory(w, r, dir, f)
 		} else {
-			serveFile(w, r, f, fi, true)
+			serveFile(w, r, f, fi.ModTime(), true)
 		}
 	})))
 }
@@ -144,116 +144,98 @@ func serveDirectory(w http.ResponseWriter, r *http.Request, dir fs.FS, f fs.File
 	// and sorting all the entries by name.
 	fd, ok := f.(fs.ReadDirFile)
 	if !ok {
-		httpError(w, os.ErrInvalid)
+		httpError(w, r, os.ErrInvalid)
 		return
 	}
 	fes, err := fd.ReadDir(0)
 	if err != nil {
-		httpError(w, err)
+		httpError(w, r, err)
 		return
 	}
-	fis := make([]fs.FileInfo, 0, len(fes))
-	for _, fe := range fes {
-		if fe.Type()&os.ModeSymlink == 0 {
-			if fi, _ := fe.Info(); fi != nil {
-				fis = append(fis, fi)
-			}
-		} else {
-			if fi, _ := fs.Stat(dir, filepath.Join(".", filepath.FromSlash(r.URL.Path), fe.Name())); fi != nil {
-				fis = append(fis, fi) // best effort symlink resolution
-			}
-		}
-	}
-	sort.Slice(fis, func(i, j int) bool {
-		return fis[i].Name() < fis[j].Name()
+	sort.Slice(fes, func(i, j int) bool {
+		return fes[i].Name() < fes[j].Name()
 	})
 
-	// Format the header.
-	var bb bytes.Buffer
-	bb.WriteString("<html lang=\"en\">\n")
-	bb.WriteString("<head>\n")
-	bb.WriteString("<title>" + html.EscapeString(r.URL.Path) + "</title>\n")
-	bb.WriteString("<style>\n")
-	bb.WriteString("body { font-family: monospace; }\n")
-	bb.WriteString("h1 { margin: 0; }\n")
-	bb.WriteString("th, td { text-align: left; }\n")
-	bb.WriteString("th, td { padding-right: 2em; }\n")
-	bb.WriteString("th { padding-bottom: 0.5em; }\n")
-	bb.WriteString("a, a:visited, a:hover, a:active { color: blue; }\n")
-	bb.WriteString("</style>\n")
-	bb.WriteString("</head>\n")
-	bb.WriteString("<body>\n")
-
-	// Format the title.
-	bb.WriteString("<h1>")
-	names := strings.Split(strings.TrimSuffix(r.URL.Path, "/"), "/")
-	for i, name := range names {
-		if i > 0 {
-			bb.WriteString(" ")
-		}
-		urlString := "." + strings.Repeat("/..", len(names)-1-i)
-		bb.WriteString(`<a href="` + html.EscapeString(urlString) + `">` + html.EscapeString(name+"/") + `</a>`)
+	type fileInfo struct {
+		Name    string
+		Size    int64
+		ModTime time.Time
 	}
-	bb.WriteString("</h1>\n")
-
-	bb.WriteString("<hr>\n")
-
-	// Format the list of files and folders.
-	bb.WriteString("<table>\n")
-	bb.WriteString("<thead>\n")
-	bb.WriteString("<tr>\n")
-	bb.WriteString("<th>Name</th>\n")
-	bb.WriteString("<th>Size</th>\n")
-	bb.WriteString("<th>Last Modified</th>\n")
-	bb.WriteString("</tr>\n")
-	bb.WriteString("</thead>\n")
-	bb.WriteString("<tbody>\n")
-	now := time.Now()
-	for _, fi := range fis {
-		name := fi.Name()
-		if fi.IsDir() {
-			name += "/"
+	var fis []fileInfo
+	for _, fe := range fes {
+		// Obtain the fs.FileInfo, resolving symbolic links if necessary.
+		var fi fs.FileInfo
+		if fe.Type()&os.ModeSymlink == 0 {
+			fi, _ = fe.Info()
+		} else {
+			fi, _ = fs.Stat(dir, filepath.Join(".", filepath.FromSlash(r.URL.Path), fe.Name()))
 		}
-		urlPath := r.URL.Path + "/" + name
-		urlString := (&url.URL{Path: name}).String()
+		if fi == nil {
+			continue
+		}
+
+		// Check whether to hide or specially handle this file.
+		urlPath := r.URL.Path + "/" + fi.Name()
 		if regexpMatch(hideRx, urlPath) || regexpMatch(denyRx, urlPath) {
 			continue
 		}
 		if regexpMatch(indexRx, urlPath) {
 			f, err := dir.Open(filepath.Join(".", filepath.FromSlash(r.URL.Path), fi.Name()))
 			if err != nil {
-				httpError(w, err)
+				httpError(w, r, err)
 				return
 			}
 			defer f.Close()
 			r.URL.Path = urlPath
-			serveFile(w, r, f, fi, false)
+			serveFile(w, r, f, fi.ModTime(), false)
 			return
 		}
-		bb.WriteString("<tr>\n")
-		bb.WriteString("<td>")
-		bb.WriteString(`<a href="` + html.EscapeString(urlString) + `">` + html.EscapeString(name) + `</a>`)
-		bb.WriteString("</td>\n")
-		bb.WriteString("<td>")
-		if fi.Mode().IsRegular() {
-			bb.WriteString(html.EscapeString(formatSize(fi.Size())))
-		}
-		bb.WriteString("</td>\n")
-		bb.WriteString("<td>")
-		bb.WriteString(html.EscapeString(formatTime(fi.ModTime(), now)))
-		bb.WriteString("</td>\n")
-		bb.WriteString("</tr>\n")
-	}
-	bb.WriteString("</tbody>\n")
-	bb.WriteString("</table>\n")
 
-	// Format the footer.
-	bb.WriteString("</body>\n")
-	bb.WriteString("</html>\n")
-	w.Write(bb.Bytes())
+		name := fi.Name()
+		if fi.IsDir() {
+			name += "/"
+		}
+		var size int64
+		if fi.Mode().IsRegular() {
+			size = fi.Size()
+		}
+		fis = append(fis, fileInfo{Name: name, Size: size, ModTime: fi.ModTime()})
+	}
+
+	// Format the list of files and folders.
+	renderHTML(w, r, func(w io.Writer) {
+		io.WriteString(w, "<table>\n")
+		io.WriteString(w, "<thead>\n")
+		io.WriteString(w, "<tr>\n")
+		io.WriteString(w, "<th>Name</th>\n")
+		io.WriteString(w, "<th>Size</th>\n")
+		io.WriteString(w, "<th>Last Modified</th>\n")
+		io.WriteString(w, "</tr>\n")
+		io.WriteString(w, "</thead>\n")
+		io.WriteString(w, "<tbody>\n")
+		now := time.Now()
+		for _, fi := range fis {
+			urlString := (&url.URL{Path: fi.Name}).String()
+			io.WriteString(w, "<tr>\n")
+			io.WriteString(w, "<td>")
+			io.WriteString(w, `<a href="`+html.EscapeString(urlString)+`">`+html.EscapeString(fi.Name)+`</a>`)
+			io.WriteString(w, "</td>\n")
+			io.WriteString(w, "<td>")
+			if !strings.HasSuffix(fi.Name, "/") {
+				io.WriteString(w, html.EscapeString(formatSize(fi.Size)))
+			}
+			io.WriteString(w, "</td>\n")
+			io.WriteString(w, "<td>")
+			io.WriteString(w, html.EscapeString(formatTime(fi.ModTime, now)))
+			io.WriteString(w, "</td>\n")
+			io.WriteString(w, "</tr>\n")
+		}
+		io.WriteString(w, "</tbody>\n")
+		io.WriteString(w, "</table>\n")
+	})
 }
 
-func serveFile(w http.ResponseWriter, r *http.Request, f fs.File, fi fs.FileInfo, allowRedirect bool) {
+func serveFile(w http.ResponseWriter, r *http.Request, f fs.File, modTime time.Time, allowRedirect bool) {
 	if allowRedirect && regexpMatch(indexRx, r.URL.Path) {
 		relativeRedirect(w, r, "./") // redirect to directory containing index.html
 		return
@@ -262,7 +244,7 @@ func serveFile(w http.ResponseWriter, r *http.Request, f fs.File, fi fs.FileInfo
 	if !ok {
 		b, err := io.ReadAll(f)
 		if err != nil {
-			httpError(w, err)
+			httpError(w, r, err)
 			return
 		}
 		rs = bytes.NewReader(b)
@@ -270,7 +252,7 @@ func serveFile(w http.ResponseWriter, r *http.Request, f fs.File, fi fs.FileInfo
 	if !*sendfile {
 		rs = struct{ io.ReadSeeker }{rs} // drop ReadFrom method to avoid using sendfile syscall
 	}
-	http.ServeContent(w, r, r.URL.Path, fi.ModTime(), rs)
+	http.ServeContent(w, r, r.URL.Path, modTime, rs)
 }
 
 func relativeRedirect(w http.ResponseWriter, r *http.Request, urlPath string) {
@@ -314,13 +296,65 @@ func formatTime(ts, now time.Time) string {
 	}
 }
 
-func httpError(w http.ResponseWriter, err error) {
+func renderHTML(w http.ResponseWriter, r *http.Request, renderBody func(io.Writer)) {
+	var bb bytes.Buffer
+	bb.WriteString("<html lang=\"en\">\n")
+	bb.WriteString("<head>\n")
+	bb.WriteString("<title>" + html.EscapeString(r.URL.Path) + "</title>\n")
+	bb.WriteString("<style>\n")
+	bb.WriteString("body { font-family: monospace; }\n")
+	bb.WriteString("h1 { margin: 0; }\n")
+	bb.WriteString("th, td { text-align: left; }\n")
+	bb.WriteString("th, td { padding-right: 2em; }\n")
+	bb.WriteString("th { padding-bottom: 0.5em; }\n")
+	bb.WriteString("a, a:visited, a:hover, a:active { color: blue; }\n")
+	bb.WriteString("</style>\n")
+	bb.WriteString("</head>\n")
+	bb.WriteString("<body>\n")
+
+	// Format the title.
+	bb.WriteString("<h1>")
+	names := strings.Split(strings.TrimSuffix(r.URL.Path, "/"), "/")
+	for i, name := range names {
+		if i > 0 {
+			bb.WriteString(" ")
+		}
+		name += "/"
+		urlString := "." + strings.Repeat("/..", len(names)-1-i)
+		if !strings.HasSuffix(r.URL.Path, "/") {
+			if i == len(names)-1 {
+				name = strings.TrimSuffix(name, "/")
+				urlString = path.Base(r.URL.Path)
+			} else {
+				urlString = strings.TrimSuffix(urlString, "/..")
+			}
+		}
+		bb.WriteString(`<a href="` + html.EscapeString(urlString) + `">` + html.EscapeString(name) + `</a>`)
+	}
+	bb.WriteString("</h1>\n")
+	bb.WriteString("<hr>\n")
+
+	renderBody(&bb)
+
+	bb.WriteString("</body>\n")
+	bb.WriteString("</html>\n")
+
+	w.Write(bb.Bytes())
+}
+
+func httpError(w http.ResponseWriter, r *http.Request, err error) {
+	var code int
 	switch {
 	case os.IsNotExist(err):
-		http.Error(w, "404 page not found", http.StatusNotFound)
+		code = http.StatusNotFound
 	case os.IsPermission(err):
-		http.Error(w, "403 Forbidden", http.StatusForbidden)
+		code = http.StatusForbidden
 	default:
-		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+		code = http.StatusInternalServerError
 	}
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.WriteHeader(code)
+	renderHTML(w, r, func(w io.Writer) {
+		io.WriteString(w, http.StatusText(code)+": "+html.EscapeString(err.Error()))
+	})
 }
